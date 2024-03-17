8c409e6763ac368115fe28a7e29f19e3b0ea5f7d
Taylan Sahin <info@taylansahin.net>
2024-03-17 00:33:59 +0000

    hide secret key ^_^

--- newsaggregator/newsaggregator/settings.py
+++ newsaggregator/newsaggregator/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*i$*$4chbkakz)qm#^r1r9gus1*ho!^hh3rb71mni_0&0ehjdi"
-
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
